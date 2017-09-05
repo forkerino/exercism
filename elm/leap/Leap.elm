dd8691bcdcc92f4e85ba49e8b9c4d81a30d061db
Pieter Stokkink <pieterstokkink@gmail.com>
2017-09-05 23:45:27 +0200

    solved elm/leap

---
+++ elm/leap/Leap.elm
@@ -0,0 +1,12 @@
+module Leap exposing (..)
+
+isLeapYear : Int -> Bool
+isLeapYear year =
+  if rem year 400 == 0 then
+    True
+  else if rem year 100 == 0 then
+    False
+  else if rem year 4 == 0 then
+    True
+  else
+    False
